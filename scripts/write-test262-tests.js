49b9c03a63256b909bad5d1b01ce727b6473a0db
Mathias Bynens <mathias@qiwi.be>
2022-09-15 13:59:22 +0200

    Ensure each start character is tested separately
    https://github.com/tc39/test262/pull/3662#discussion_r970740983

--- scripts/write-test262-tests.js
+++ scripts/write-test262-tests.js
@@ -1,68 +1,92 @@
 const fs = require('fs');
 const jsesc = require('jsesc');
 
-const template = require('lodash.template');
+const _template = require('lodash.template');
+
+const escapeIdentifier = (identifier) => {
+	return jsesc(identifier, {
+		wrap: false,
+		es6: true,
+	});
+};
+
 const TEMPLATE_OPTIONS = {
 	interpolate: /<%=([\s\S]+?)%>/g,
+	imports: {
+		escapeIdentifier,
+	},
 };
+const template = (input) => {
+	const fn = _template(input, TEMPLATE_OPTIONS);
+	return fn;
+};
+
+const fixLineEndings = (string) => {
+	return string.replaceAll(';\n\n}', ';\n}').trimEnd() + '\n';
+};
+
 const TEST_VAR_TEMPLATE = fs.readFileSync('./templates/var.template', 'utf8');
-const createVarTest = template(TEST_VAR_TEMPLATE, TEMPLATE_OPTIONS);
+const createVarTest = template(TEST_VAR_TEMPLATE);
 const TEST_CLASS_TEMPLATE = fs.readFileSync('./templates/class.template', 'utf8');
-const createClassTest = template(TEST_CLASS_TEMPLATE, TEMPLATE_OPTIONS);
+const createClassTest = template(TEST_CLASS_TEMPLATE);
 const TEST_VAR_ESCAPED_TEMPLATE = fs.readFileSync('./templates/var-escaped.template', 'utf8');
-const createVarEscapedTest = template(TEST_VAR_ESCAPED_TEMPLATE, TEMPLATE_OPTIONS);
+const createVarEscapedTest = template(TEST_VAR_ESCAPED_TEMPLATE);
 const TEST_CLASS_ESCAPED_TEMPLATE = fs.readFileSync('./templates/class-escaped.template', 'utf8');
-const createClassEscapedTest = template(TEST_CLASS_ESCAPED_TEMPLATE, TEMPLATE_OPTIONS);
+const createClassEscapedTest = template(TEST_CLASS_ESCAPED_TEMPLATE);
 
 const writeTest262Tests = ({ version, identifierStart, identifierPart }) => {
 
 	// Create IdentifierStart tests.
 
-	fs.writeFileSync(`./output/test262/start-unicode-${version}.js`, createVarTest({
+	fs.writeFileSync(`./output/test262/start-unicode-${version}.js`, fixLineEndings(createVarTest({
 		version: version,
 		property: 'ID_Start',
 		identifier: identifierStart,
-	}));
-	fs.writeFileSync(`./output/test262/start-unicode-${version}-class.js`, createClassTest({
+	})));
+	fs.writeFileSync(`./output/test262/start-unicode-${version}-class.js`, fixLineEndings(createClassTest({
 		version: version,
 		property: 'ID_Start',
 		identifier: identifierStart,
-	}));
+	})));
 
-	fs.writeFileSync(`./output/test262/start-unicode-${version}-escaped.js`, createVarEscapedTest({
+	fs.writeFileSync(`./output/test262/start-unicode-${version}-escaped.js`, fixLineEndings(createVarEscapedTest({
 		version: version,
 		property: 'ID_Start',
-		escapedIdentifier: jsesc(identifierStart, { wrap: false, es6: true, }),
-	}));
-	fs.writeFileSync(`./output/test262/start-unicode-${version}-class-escaped.js`, createClassEscapedTest({
+		identifier: identifierStart,
+		escapedIdentifier: null,
+	})));
+	fs.writeFileSync(`./output/test262/start-unicode-${version}-class-escaped.js`, fixLineEndings(createClassEscapedTest({
 		version: version,
 		property: 'ID_Start',
-		escapedIdentifier: jsesc(identifierStart, { wrap: false, es6: true, }),
-	}));
+		identifier: identifierStart,
+		escapedIdentifier: null,
+	})));
 
 	// Create IdentifierPart tests.
 
-	fs.writeFileSync(`./output/test262/part-unicode-${version}.js`, createVarTest({
+	fs.writeFileSync(`./output/test262/part-unicode-${version}.js`, fixLineEndings(createVarTest({
 		version: version,
 		property: 'ID_Continue',
 		identifier: identifierPart,
-	}));
-	fs.writeFileSync(`./output/test262/part-unicode-${version}-class.js`, createClassTest({
+	})));
+	fs.writeFileSync(`./output/test262/part-unicode-${version}-class.js`, fixLineEndings(createClassTest({
 		version: version,
 		property: 'ID_Continue',
 		identifier: identifierPart,
-	}));
+	})));
 
-	fs.writeFileSync(`./output/test262/part-unicode-${version}-escaped.js`, createVarEscapedTest({
+	fs.writeFileSync(`./output/test262/part-unicode-${version}-escaped.js`, fixLineEndings(createVarEscapedTest({
 		version: version,
 		property: 'ID_Continue',
-		escapedIdentifier: jsesc(identifierPart, { wrap: false, es6: true, }),
-	}));
-	fs.writeFileSync(`./output/test262/part-unicode-${version}-class-escaped.js`, createClassEscapedTest({
+		identifier: null,
+		escapedIdentifier: escapeIdentifier(identifierPart),
+	})));
+	fs.writeFileSync(`./output/test262/part-unicode-${version}-class-escaped.js`, fixLineEndings(createClassEscapedTest({
 		version: version,
 		property: 'ID_Continue',
-		escapedIdentifier: jsesc(identifierPart, { wrap: false, es6: true, }),
-	}));
+		identifier: null,
+		escapedIdentifier: escapeIdentifier(identifierPart),
+	})));
 
 };
 
